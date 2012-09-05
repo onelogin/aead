0b46e6ce00e1f354e28c92e329b925dd0f22dd06
Stephen Touset <stephen@touset.org>
2012-09-05 14:35:59 -0700

    Filter out /vendor/ in SimpleCov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ SimpleCov.start do
   command_name 'MiniTest'
   add_filter   '/.bundle/'
   add_filter   '/spec/'
+  add_filter   '/vendor/'
 end unless defined?(RUBY_ENGINE) and RUBY_ENGINE == 'rbx'
 
 require 'minitest/autorun'
